6e43d95ec8ea674174d7eb0c2e30efb49e300d81
VkMishra <vineetmishra2451@gmail.com>
2020-05-05 15:29:06 +0530

    Added different shapes

--- Key-Sound Player/main.js
+++ Key-Sound Player/main.js
@@ -1,6 +1,6 @@
 var data = {};
 var ch = 'a';
-var w,h,x,y;
+var w,h,x,y,shape;
 var colors=["","teal","yellow","steelblue","yellowgreen","orange","gray","pink","salmon","chocolate","mistyrose","hotpink","red","tomato","beige","coral","orchid","olive","blue","lightpink","lightgreen","skyblue","aqua","brown","lime","khaki","purple"]
 
 for(var i=1;i<=26;i++){
@@ -21,20 +21,52 @@ function onKeyDown(event){
     h = view.size.height;
     x = Math.random()*w;
     y = Math.random()*h;
-    var circle = new Path.Circle(new Point(x,y),300);
+    shape = Math.floor(Math.random()*4 + 1);
+    if(shape==1){
+    var circle = new Path.Circle(new Point(x,y),500);
     circle.fillColor = data[event.key].color;
     data[event.key].sound.play();
-    circleArray.push(circle);
+    mainArray.push(circle);
+    }
+    else if(shape==2){
+        var rectangle = new Rectangle(new Point(x, y), new Point(x+500,y+300));
+        var radius = new Size(10, 10);
+        var path = new Path.Rectangle(rectangle, radius);
+        path.fillColor = data[event.key].color;
+        data[event.key].sound.play();
+        mainArray.push(path);
+    }
+    else if(shape==3){
+        var center = new Point(x, y);
+        var sides = 6;
+        var radius = 400;
+        var path = new Path.RegularPolygon(center, sides, radius);
+        path.fillColor = data[event.key].color;
+        data[event.key].sound.play();
+        mainArray.push(path);
+    }
+    else if(shape==4){
+        var center = new Point(x, y);
+        var points = 26;
+        var radius1 = 325;
+        var radius2 = 540;
+        var path = new Path.Star(center, points, radius1, radius2);
+        path.fillColor = data[event.key].color;
+        data[event.key].sound.play();
+        mainArray.push(path);
+
+    }
+    
 }
 }
-var circleArray = [];
+var mainArray = [];
 function onFrame(){
-    for(var i=0;i<circleArray.length;i++){
-        circleArray[i].scale(0.9);
-        circleArray[i].fillColor.hue+=5; 
-        if(circleArray[i].area<1){
-            circleArray[i].remove();
-            circleArray.splice(i,1);
+    for(var i=0;i<mainArray.length;i++){
+        mainArray[i].scale(0.9);
+        mainArray[i].fillColor.hue+=5; 
+        if(mainArray[i].area<1){
+            mainArray[i].remove();
+            mainArray.splice(i,1);
         } 
     }
           
